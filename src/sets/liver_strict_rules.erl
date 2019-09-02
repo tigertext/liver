bd8ca4aba42c9cb8a8829ebf5f9e2afa89714628
Anders Wei <anders.wei@outlook.com>
2019-09-02 15:18:54 +0800

    add boolean checks

--- src/sets/liver_strict_rules.erl
+++ src/sets/liver_strict_rules.erl
@@ -84,14 +84,14 @@ is_integer(_Args, _Value, _Opts) ->
 
 is_boolean(_Args, Value, _Opts) when is_boolean(Value) ->
     {ok, Value};
-is_boolean(_Args, <<"true">> = Value, _Opts) ->
-    {ok, Value};
-is_boolean(_Args, <<"false">> = Value, _Opts) ->
-    {ok, Value};
-is_boolean(_Args, <<"1">> = Value, _Opts) ->
-    {ok, Value};
-is_boolean(_Args, <<"0">> = Value, _Opts) ->
-    {ok, Value};
+is_boolean(_Args, <<"true">>, _Opts) ->
+    {ok, true};
+is_boolean(_Args, <<"false">>, _Opts) ->
+    {ok, false};
+is_boolean(_Args, <<"1">>, _Opts) ->
+    {ok, true};
+is_boolean(_Args, <<"0">>, _Opts) ->
+    {ok, false};
 is_boolean(_Args, _Value, _Opts) ->
     {error, not_boolean}.
 
